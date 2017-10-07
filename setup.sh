48a1bce0c765bcd4d8c230ce03a260385a7b24f2
Magnus Lång <margnus1@telia.com>
2017-10-07 22:35:57 +0200

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Emacs
+mkdir -p ~/.emacs.d/
+ln -s emacs/* ~/.emacs.d/
+
+# Fish
+mkdir -p ~/.config/fish/
+ln -s fish/* ~/.config/fish/
+
+# mpv
+mkdir -p ~/.config/mpv/
+ln -s mpv/* ~/.config/mpv/
